dd765a0589ccb48eed235cd3aa0577d3c6ada62b
Alexmv235 <a.m.v22305@gmail.com>
2023-07-07 18:38:48 -0600

    entregaSesion1

---
+++ JAVA BASICO/sesion1/src/com/entregasesionuno/MainEntregaUno.java
@@ -0,0 +1,44 @@
+package com.entregasesionuno;
+
+public class MainEntregaUno {
+    /**
+     * Metodo main de la clase MainEntregaUno en donde se
+     * utiliziaraa el metodos System.out.prinln() para imprimir
+     * en la consola todos los metodos vistos
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        //Tipos numericos
+        //Numericos enteros
+
+        byte variableByte= 3;
+        System.out.println(variableByte);
+        short variableShort=12;
+        System.out.println(variableShort);
+        int variableEntera=54;
+        System.out.println(variableEntera);
+        long variableLong=1023;
+        System.out.println(variableLong);
+
+        //Numericos decimales
+        float variableFlotante= 5.87f;
+        System.out.println(variableFlotante);
+        double variableDouble=8.9356;
+        System.out.println(variableDouble);
+
+        //Booleanos
+        boolean variableT = true;
+        boolean variableF= false;
+        System.out.println(variableT);
+        System.out.println(variableF);
+
+        //Texto
+        char variableChar ='c';
+        System.out.println(variableChar);
+        String variablePalabra = "Hola Mundo!";
+        System.out.println(variablePalabra);
+        
+    }
+    
+}
